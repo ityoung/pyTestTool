ec5367066cdc1d33e4cbf30999b93d4dc9d707b7
ityoung <ityoung@126.com>
2016-05-19 14:20:25 +0800

    finished the function 1 & 2, with multithread and subprocess, now runs absolutely what I need!

--- wnd_design.py
+++ wnd_design.py
@@ -1,113 +1,143 @@
-import signal
+import threading
+import subprocess
 import wx
 import os
-import subprocess
-import sys
-import time
 import re
 
-#from StartMRService import StartMRService
-
+class showLogThread(threading.Thread):
+    def __init__(self, window, cmd):
+        threading.Thread.__init__(self)
+        self.window = window
+        self.cmd = cmd
+        self.timeToQuit = threading.Event()
+        self.timeToQuit.clear()
+    def stop(self):
+        self.flogcat.terminate()
+        self.timeToQuit.set()
+    def run(self):
+        self.window.logMessageText.Clear()
+        self.flogcat = subprocess.Popen(self.cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, creationflags=0x08)
+        while True:
+            output = self.flogcat.stdout.readline()
+            if self.window.packageName!="":
+                if re.search(self.window.packageName,output):
+                    self.window.logMessageText.AppendText(output)
+            else:
+                self.window.logMessageText.AppendText(output)
+                print output
+            if self.timeToQuit.isSet():
+                break
+    
+class checkDeviceThread(threading.Thread):
+    def __init__(self, window):
+        threading.Thread.__init__(self)
+        self.window = window
+        self.timeToQuit = threading.Event()
+        self.timeToQuit.clear()
+    def stop(self):
+        self.timeToQuit.set()
+    def run(self):
+        while 1:
+            checkdevicecmd = "adb devices"
+            deviceLine = os.popen(checkdevicecmd).readlines()
+            if len(deviceLine)==2:
+                self.window.isconn = False
+                self.timeToQuit.wait(0.1)
+                wx.CallAfter(self.window.LogMessage)
+            else:
+                self.window.isconn = True
+                self.timeToQuit.wait(0.1)
+                wx.CallAfter(self.window.LogMessage)
+            if self.timeToQuit.isSet():
+                break
+         
 class TestTool(object):
     def __init__(self):
-        self.wndSize = wx.Size(600,400)
+        self.wndSize = wx.Size(550,400)
+        thread = checkDeviceThread(self)
+        thread.setDaemon(True)      #terminate child thread when main thread ends.
+        thread.start()              #check devices until die
+        
+        self.isCatting = False
+        self.isconn = False
+        self.listenDevice = True
+        self.packageName =""
 
     def show(self):
-        app = wx.App()
         self.frame = wx.Frame(None,title="testTool", size=self.wndSize)  
         panel = wx.Panel(self.frame, 1)
         
         #-------------- button ---------------
-        self.connButton = wx.Button(panel, -1, "show device info", pos=(10, 10))
-        self.connButton.SetDefault()
+        #self.connButton = wx.Button(panel, -1, "show device info", pos=(10, 10))
+        #self.connButton.SetDefault()
         self.logClearBtn = wx.Button(panel, -1, "logcat clear", pos = (220, 10))
         self.logClearBtn.SetDefault()
         self.logCatAllBtn = wx.Button(panel, -1, "logcat all", pos = (320, 10))
         self.logCatSelectBtn = wx.Button(panel, -1, "logcat select", pos = (420, 10))
         
         #self.logClearBtn.Disable()
-#         self.logCatAllBtn.Disable()
-#         self.logCatSelectBtn.Disable()
+        #self.logCatAllBtn.Disable()
+        #self.logCatSelectBtn.Disable()
         
         #-------------- text view ---------------
-        self.deviceInfoText = wx.TextCtrl(panel, -1, pos=(10,50),size=(200,50),style=wx.TE_MULTILINE|wx.TE_NO_VSCROLL)
+        self.deviceInfoText = wx.TextCtrl(panel, -1, pos=(10,50),size=(200,70),style=wx.TE_MULTILINE|wx.TE_NO_VSCROLL)
         self.deviceInfoText.SetEditable(False)
-        self.packgeNameText = wx.TextCtrl(panel, -1, pos = (220, 50), size=(300,300),style=wx.TE_MULTILINE|wx.TE_LINEWRAP)
+        self.logMessageText = wx.TextCtrl(panel, -1, pos = (220, 50), size=(300,300),style=wx.TE_MULTILINE|wx.TE_LINEWRAP)
         
         #-------------- bind -------------
-        self.connButton.Bind(wx.EVT_BUTTON,self.clickConn)
+        #self.connButton.Bind(wx.EVT_BUTTON,self.clickConn)
         self.logClearBtn.Bind(wx.EVT_BUTTON, self.logClear)
         self.logCatAllBtn.Bind(wx.EVT_BUTTON, self.logCatAll)
         self.logCatSelectBtn.Bind(wx.EVT_BUTTON, self.logCatSelect)
         
-        self.isCatting = False
-        self.packageName =""
-        
         self.frame.Show()
-        app.MainLoop()
-        
-    def clickConn(self,even):
-        checkdevicecmd = "adb devices"
-        isconn = os.popen(checkdevicecmd).readlines()
-        if len(isconn) ==3:
+    
+    def LogMessage(self):
+        if self.isconn == True and self.listenDevice == True:
+            self.listenDevice = False
+            self.deviceInfoText.SetValue("Connected!\n")
+            self.logClearBtn.Enable()
+            self.logCatAllBtn.Enable()
+            self.logCatSelectBtn.Enable()
             getdeviceinfocmd = 'adb shell "cat /system/build.prop | grep \"product\""'
             deviceinfo = os.popen(getdeviceinfocmd).read()
             device_brand = re.findall('ro.product.brand=(.*)', deviceinfo)
             device_model = re.findall('ro.product.model=(.*)', deviceinfo)
-            self.deviceInfoText.SetValue("device brand:"+device_brand[0]+"\ndevice model:"+device_model[0])
-            self.connButton.SetLabel("Connected")
-            self.connButton.Disable()
-            self.logClearBtn.Enable()
-            self.logCatAllBtn.Enable()
-            self.logCatSelectBtn.Enable()
-        else:
-            dialog = wx.MessageDialog(self.frame,"please connect your device!",style=wx.OK_DEFAULT)
-            dialog.ShowModal()
-            self.deviceInfoText.Clear()
-            
-    def startAppium(self,even):
-        import os
-        appium = os.system('adb logcat > E:/log/a.txt')
-        self.appiumRunText.SetValue(appium)
-        #handler = AppiumLogHandler(self.appiumRunText)
-        #logging.getLogger().addHandler(handler)
+            self.deviceInfoText.AppendText("device brand:"+device_brand[0]+"\ndevice model:"+device_model[0])
+        elif self.isconn == False and self.listenDevice == False:
+            self.listenDevice = True
+            self.deviceInfoText.SetValue("Disconnected!")
             
     def logClear(self,even):
         dlg = wx.MessageDialog(None, 'Confirm clear old-log?',
                           'Warning', wx.YES_NO | wx.ICON_QUESTION)
         result = dlg.ShowModal()
-        # if choose no then exit
-        if result == wx.ID_NO:
-            os.system("explorer E:/log")
-            pass
-        else:
+        if result == wx.ID_YES:
             cmd = "adb logcat -c"
             os.system(cmd)
+            self.logMessageText.SetValue("Clear log successfully!")
         dlg.Destroy()
             
     def logCatAll(self,even):
         if self.isCatting == False:
-            self.packgeNameText.Clear()
-            filename = "E:/log/t.txt"
-            #fp = open(filename, 'w+')
-            cmd = 'adb logcat -v time'
-            self.flogcat = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, creationflags=0x08)
-            #self.flogcat = subprocess.Popen(cmd,stdout = fp, stderr=subprocess.PIPE)
             self.isCatting = True
             self.logCatAllBtn.SetLabel("Stop logcat")
             self.logClearBtn.Disable()
             self.logCatSelectBtn.Disable()
-            wx.CallAfter(self.doLogging)
+            cmd = 'adb logcat -v time'
+            self.flogcat = showLogThread(self, cmd)
+            self.flogcat.setDaemon(True)
+            self.flogcat.start()
         else:
-            self.flogcat.terminate()
             self.isCatting = False
             self.logCatAllBtn.SetLabel("logcat all")
             self.logClearBtn.Enable()
             self.logCatSelectBtn.Enable()
-    
+            self.flogcat.stop()
+            
     def logCatSelect(self,even):
         if self.isCatting == False:
-            self.packgeNameText.Clear()
+            self.logMessageText.Clear()
             getpackagename = "adb shell \"dumpsys window windows | grep -E 'mFocusedApp'\""
             result = os.popen(getpackagename).readlines()
             cur_pkg = re.findall('com.*/', result[0])
@@ -118,38 +148,22 @@ class TestTool(object):
                 self.packageName = dlg.GetValue()
                 dlg.Destroy()
                 self.isCatting = True
-                filename = "E:/log/ttt.txt"
-                fp = open(filename, 'w+')
-                cmd = 'adb shell logcat -v time'
-                #cmd = 'adb shell "logcat -v time|grep '+self.packageName+'"'
-                self.flogcat = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, creationflags=0x08)
+                cmd = 'adb shell "logcat -v time|grep '+self.packageName+'"'
+                self.flogcat = showLogThread(self, cmd)
                 self.logCatSelectBtn.SetLabel("Stop logcat")
                 self.logClearBtn.Disable()
                 self.logCatAllBtn.Disable()
-                wx.CallAfter(self.doLogging)
-            else:
-                return
+                self.flogcat.setDaemon(True)
+                self.flogcat.start()
         else:
-            self.flogcat.terminate()
             self.packageName=""
             self.isCatting = False
             self.logCatSelectBtn.SetLabel("logcat select")
             self.logClearBtn.Enable()
             self.logCatAllBtn.Enable()
-
-    def doLogging(self):
-        if self.isCatting:
-            try:
-                output = self.flogcat.stdout.readline()
-                if self.packageName!="":
-                    if re.search(self.packageName,output):
-                        self.packgeNameText.AppendText(output)
-                else:
-                    self.packgeNameText.AppendText(output)
-                    print output
-                wx.CallLater(100,self.doLogging)
-            except:
-                return
-
+            self.flogcat.stop()
+            
 if __name__ == '__main__':
+    app = wx.App()
     TestTool().show()
+    app.MainLoop()
