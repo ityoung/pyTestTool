86e22a63875625f4eeb7ff8d3031a36386ae17cf
ityoung <ityoung@126.com>
2016-05-30 17:58:01 +0800

    add performancetesting page and code for monitor cpu/memory utilization of a certain app. also solve an error of start appium function.

--- wnd_design.py
+++ wnd_design.py
@@ -6,6 +6,37 @@ import threading
 import subprocess
 import time
 
+class monitorPerformanceThread(threading.Thread):
+    def __init__(self, window):
+        threading.Thread.__init__(self)
+        self.window      = window
+        self.packagename = self.window.packagename
+        self.timeToQuit  = threading.Event()
+        self.timeToQuit.clear()
+        self.cmd = "adb shell \"top |grep "
+        
+    def stop(self):
+        self.fstart.terminate()
+        self.timeToQuit.set()
+        
+    def run(self):
+        print self.cmd+self.packagename+'"'
+        self.window.cpuUtilization.Clear()
+        self.window.memUtilization.Clear()
+        self.fstart = subprocess.Popen(self.cmd+self.packagename+'"', 
+                                       stdin=subprocess.PIPE, 
+                                       stdout=subprocess.PIPE, 
+                                       stderr=subprocess.PIPE, 
+                                       creationflags=0x08)
+        while True:
+            output = self.fstart.stdout.readline()
+            opArray = output.split()
+            print opArray
+            self.window.cpuUtilization.AppendText(opArray[2]+'\t'+opArray[-1]+"\n")
+            self.window.memUtilization.AppendText("VSS:"+opArray[5]+'\tRSS:'+opArray[6]+'\t'+opArray[-1]+"\n")
+            if self.timeToQuit.isSet():
+                break
+
 class listenTouchThread(threading.Thread):
     def __init__(self, window):
         threading.Thread.__init__(self)
@@ -33,7 +64,6 @@ class listenTouchThread(threading.Thread):
                 if " 0036 " in read:
                     y_start = read.split()[3]
                     read = self.fstart.stdout.readline()
-        #             print hex2dec('0000021b')
                 x_end = x_start
                 y_end = y_start
                 while " 0039 " not in read:
@@ -49,12 +79,6 @@ class listenTouchThread(threading.Thread):
                                                        self.hex2dec(y_start), \
                                                        self.hex2dec(x_end), \
                                                        self.hex2dec(y_end))+"\n")
-#                 self.window.recordedScript.AppendText('\n'+self.hex2dec(x_start)\
-#                                                       +' '+self.hex2dec(y_start)\
-#                                                       +' '+self.hex2dec(x_end)\
-#                                                       +' '+self.hex2dec(y_end)+'\n')
-#                 print int((time2-time1)*1000)
-                #self.window.recordedScript.AppendText(str(('%.3f'%(time2-time1))*1000))
             if self.timeToQuit.isSet():
                 break
     def recorde(self, language, time, \
@@ -80,9 +104,9 @@ class startAppiumThread(threading.Thread):
         self.window.appiumLog.Clear()
         self.window.appiumLog.SetValue("running...")
         try:
+            os.system("taskkill /f /t /im node.exe")
             self.fstart = subprocess.Popen(self.startCmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, creationflags=0x08)
         except:
-            os.system("taskkill /f /t /im node.exe")
             self.fstart = subprocess.Popen(self.startCmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, creationflags=0x08)
         while True:
             output = self.fstart.stdout.readline()
@@ -145,7 +169,6 @@ class updateScreenThread(threading.Thread):
         self.timeToQuit.clear()
         self.cmd1 = "adb shell /system/bin/screencap -p /sdcard/sstemp.png"
         self.cmd2 = "adb pull /sdcard/sstemp.png F://"
-        image2 = wx.Image("F://welcome.png", wx.BITMAP_TYPE_PNG, index=-1)
         self.w = 270
         self.h = 490
 #         image = image2.Scale(self.w,self.h)
@@ -169,14 +192,14 @@ class mainFrame(wx.Frame):
         self.SetSize((900,605))
         
 #--------------- split window ---------------
-        self.sp = wx.SplitterWindow(self, id=-1)
+        self.sp     = wx.SplitterWindow(self, id=-1)
         self.panel1 = wx.Panel(self.sp, style=wx.SUNKEN_BORDER)
         self.panel2 = wx.Panel(self.sp, style=wx.SUNKEN_BORDER)
         self.sp.Initialize(self.panel1)
         self.sp.SplitVertically(self.panel1, self.panel2, 285)
         
 #--------------- device info ----------------        
-        self.deviceInfoText = wx.TextCtrl(self.panel1, -1,size=(200,70),style=wx.TE_MULTILINE|wx.TE_NO_VSCROLL)
+        self.deviceInfoText  = wx.TextCtrl(self.panel1, -1,size=(200,70),style=wx.TE_MULTILINE|wx.TE_NO_VSCROLL)
         self.listenScreenBtn = wx.Button(self.panel1, -1, "show screen\nReal-tiem",size=(80,70))
         thread = checkDeviceThread(self)
         thread.setDaemon(True)      #terminate child thread when main thread ends.
@@ -192,7 +215,6 @@ class mainFrame(wx.Frame):
         global isConn
         isConn = False
         self.isUpdateScreen = False
-        image2 = wx.Image("F://welcome.png", wx.BITMAP_TYPE_PNG, index=-1)
         self.w = 270
         self.h = 490
         
@@ -201,6 +223,7 @@ class mainFrame(wx.Frame):
         notebook.AddPage(logTool(notebook), "logTool")
         notebook.AddPage(appiumTool(notebook), "appiumTool")
         notebook.AddPage(recordeTool(notebook), "recordeTool")
+        notebook.AddPage(performanceTestTool(notebook), "performanceTest")
         p_sizer = wx.BoxSizer(wx.HORIZONTAL)
         p_sizer.Add(notebook, 1, wx.EXPAND | wx.ALL, 7) 
         self.panel2.SetSizer(p_sizer)
@@ -232,6 +255,61 @@ class mainFrame(wx.Frame):
             self.listenScreenBtn.SetLabel("show screen\nReal-tiem")
             self.isUpdateScreen = False
             self.fupdateScreen.stop()
+     
+class performanceTestTool(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self,parent)
+        self.isMonitoring = False
+        self.packagename  = ''
+        
+        #--------- BTN ----------
+        self.getCurPackagenameBtn = wx.Button(self, -1, "get current packagename")
+        self.startMonitorBtn       = wx.Button(self, -1, "start listen")
+        
+        #--------- TEXT ---------
+        self.packagenameText = wx.TextCtrl(self, -1, size=(180,28))
+        self.cpuUtilization  = wx.TextCtrl(self, -1, size=(250,300), style=wx.TE_MULTILINE)
+        self.memUtilization  = wx.TextCtrl(self, -1, size=(440,300), style=wx.TE_MULTILINE)
+        
+        #--------- BIND ---------
+        self.getCurPackagenameBtn.Bind(wx.EVT_BUTTON, self.getCurPackagename)
+        self.startMonitorBtn.Bind(wx.EVT_BUTTON, self.startMonitor)
+        
+        #--------- sizer --------
+        line1_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        line1_sizer.Add(self.getCurPackagenameBtn, 0, 0)
+        line1_sizer.Add(self.packagenameText, 1, wx.EXPAND)
+        line1_sizer.Add(self.startMonitorBtn, 0, 0)
+        
+        line2_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        line2_sizer.AddMany([(self.cpuUtilization, 0, wx.EXPAND), (self.memUtilization, 0, wx.EXPAND)])
+        
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.AddMany([line1_sizer, line2_sizer])
+        self.SetSizer(sizer)
+        
+    def getCurPackagename(self, evt):
+        cmd = "adb shell \"dumpsys window windows | grep -E 'mFocusedApp'\""
+        result = os.popen(cmd).readlines()
+        cur_pkg = re.findall('u0 .*/', result[0])
+        cur_pkg[0] = cur_pkg[0].strip('u0 ')
+        cur_pkg[0] = cur_pkg[0].strip('/')
+        self.packagename = cur_pkg[0]
+        self.packagenameText.SetValue(self.packagename)
+        
+    def startMonitor(self, evt):
+        if self.isMonitoring == False:
+            self.fstart = monitorPerformanceThread(self)
+            self.fstart.setDaemon(True)
+            self.fstart.start()
+            self.startMonitorBtn.SetLabel("stop monitor")
+            self.getCurPackagenameBtn.Disable()
+            self.isMonitoring = True
+        else:
+            self.fstart.stop()
+            self.startMonitorBtn.SetLabel("start monitor")
+            self.getCurPackagenameBtn.Enable()
+            self.isMonitoring = False
      
 class recordeTool(wx.Panel):
     def __init__(self, parent):
@@ -277,7 +355,7 @@ class recordeTool(wx.Panel):
      
     def languageChoice(self, evt):
         self.recordeLanguage = self.choice.GetString(self.choice.GetSelection())
-#         print self.recordeLaguage
+
 class appiumTool(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
@@ -344,9 +422,10 @@ class appiumTool(wx.Panel):
         if scriptPath!="":
             path = scriptPath.split("/")
             scriptpath = scriptPath.strip(path[-1])
+            print scriptpath+scriptPath
             import sys
-            sys.path.append(path)
-            execfile(scriptPath)
+            sys.path.append(r"F:\mhome\mhome_test")
+            os.system("python "+scriptpath+scriptPath)
         else:
             dlg = wx.MessageDialog(None,"warning!","Empty Path!",wx.OK|wx.ICON_WARNING)
             dlg.ShowModal()
